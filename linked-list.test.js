bf2285c1cbcb1226163ed56ab5c80b41ddcd5baf
Ivan Labiak <labyak.ivan2003@gmail.com>
2022-02-18 14:51:05 +0200

    made unit tests for getLength and append methods

--- linked-list.test.js
+++ linked-list.test.js
@@ -1,5 +1,21 @@
 const DoubleLinkedList = require('./linked-list');
 
+test('getLength method test', () => {
+    const list = new DoubleLinkedList();
+    expect(list.getLength()).toBe(0)
+    list.append('f')
+    expect(list.getLength()).toBe(1)
+    list.append('a')
+    list.append('b')
+    list.append('c')
+    expect(list.getLength()).toBe(4)
+    list.delete(3)
+    list.delete(1)
+    expect(list.getLength()).toBe(2)
+    list.clear()
+    expect(list.getLength()).toBe(0)
+  });
+
 
 test('Append to list wrong arguments', () => {
     const list = new DoubleLinkedList();
@@ -8,5 +24,14 @@ test('Append to list wrong arguments', () => {
     expect(() => list.append('fff')).toThrow(Error)
     expect(() => list.append()).toThrow(Error)
     expect(() => list.append(f)).toThrow(Error)
+  });
+
+  test('Append to list rigth arguments', () => {
+    const list = new DoubleLinkedList();
+    expect(list.append('1')).toBe(undefined)
+    expect(list.append('.')).toBe(undefined)
+    expect(list.append('a')).toBe(undefined)
+    expect(list.append('-')).toBe(undefined)
+    expect(list.append('!')).toBe(undefined)
   });
 
