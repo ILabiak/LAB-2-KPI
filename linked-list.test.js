0d95dc9726e761a0b3f6ab129333ef4275cfc8a7
Ivan Labiak <labyak.ivan2003@gmail.com>
2022-02-18 14:11:44 +0200

    added some tests

--- linked-list.test.js
+++ linked-list.test.js
@@ -1,8 +1,12 @@
-const { test } = require('@jest/globals');
 const DoubleLinkedList = require('./linked-list');
 
-const list = new DoubleLinkedList();
-list.append('1')
-list.append('f')
-list.showList()
+
+test('Append to list wrong arguments', () => {
+    const list = new DoubleLinkedList();
+    expect(() => list.append(1)).toThrow(Error)
+    expect(() => list.append(123)).toThrow(Error)
+    expect(() => list.append('fff')).toThrow(Error)
+    expect(() => list.append()).toThrow(Error)
+    expect(() => list.append(f)).toThrow(Error)
+  });
 
